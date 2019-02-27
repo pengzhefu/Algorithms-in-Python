8f948680f8666444fe00943d31074a9c8e60ae9d
pengzhefu <32311379+pengzhefu@users.noreply.github.com>
2019-02-27 01:59:39 -0500

    Create Trees_baisc.py

---
+++ Trees_baisc.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 27 01:25:26 2019
+
+@author: pengz
+"""
+
+###########Trees############
+   ######用列表表示树########
+myTree = ['a',   #root
+      ['b',  #left subtree
+       ['d', [], []],
+       ['e', [], []] ],
+      ['c',  #right subtree
+       ['f', [], []],
+       [] ]
+     ]
+#print(myTree)
+print('left subtree = ', myTree[1])
+print('root = ', myTree[0])
+print('right subtree = ', myTree[2])
+
+def BinaryTree(r):
+    return [r, [], []]  ##跟r同一个level，紧贴着r后面的逗号的括号里的内容，就是左子树和有子树的内容
+                        ##一般一个点后面会跟两个list:(a,[],[])这是一个完整的节点，说明这个节点没有子节点，为空，
+                        ##但是空的也要把[]带上
+#print(BinaryTree('a')[2])
+def insertLeft(root,newBranch): ##root可以是你要插进去的那个Tree
+                                ##这个方法是往离你要插的点的下一层插一个点（左或右，这个是左）
+    t = root.pop(1)
+    if len(t) > 1:
+        root.insert(1,[newBranch,t,[]]) ##原来的点的左子树（包括原来那个点本身）变成新加上去的点的左子树
+    else:
+        root.insert(1,[newBranch, [], []]) ##没有的话就加上一个
+    return root ##root其实就是整个tree
+
+def insertRight(root,newBranch):   ##just like the insertleft
+    t = root.pop(2)
+    if len(t) > 1:
+        root.insert(2,[newBranch,[],t])
+    else:
+        root.insert(2,[newBranch,[],[]])
+    return root
+
+def getRootVal(root): 
+    return root[0]
+
+def setRootVal(root,newVal): ##更改根节点的值
+    root[0] = newVal
+
+def getLeftChild(root):
+    return root[1]
+
+def getRightChild(root):
+    return root[2]
+
+
+a = BinaryTree('a')
+print(a)
+print(a[0])
+b= insertLeft(a,'b')
+print(b)
+print(b[0])
+print(b[1])
+print(b[2])
+c = insertRight(b,'c')
+print(c)
+print(c[1])
+print(c[2])
+d= insertLeft(c,'d')
+print(d)
+print(d[1])
+print(d[2])
+e = insertRight(d,'e')
+print(e)
+print(e[1])
+print(e[2])
+f = insertRight(a[1],'f')  ##这样相当于给d点添加一个右节点f
+print(a)
+print(a[1][1][0])   ##打印出a的左节点的左节点的不带子点，相当于b
+print(a[1][2])   ##打印出a的左节点的右节点带上子点，相当于f
+print(a[2][0])   ##打印出a的右节点的唯一的点不带子点，相当于e
+print(a[2][2])   ##打印出a的右节点的右节点带上子点，相当于c
+g = insertLeft(a[2],'g') ##给e点添加一个左节点g
+print(a)
+o = insertRight(a[2][1],'o') ##给g点添加一个左节点o
+print(a)
+t = insertLeft(a[1],'t')
+print(a)
+print('#######################################')
+print(getLeftChild(a[2]))
+print(getRootVal(d))
